f81b3e929b519aff8b6dfa5b6ca009da6134736e
Zzigol <zigolenko@rambler.ru>
2022-05-13 22:39:06 +0300

    задание 2 готово к проверке

---
+++ Recursia2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Recursia
+{
+
+   
+    // 2. Найти сумму элементов массива с помощью рекурсии.
+   
+
+    class program
+    {
+
+        static int Recursia(int[] myArray,ref int sum, int i)
+        {
+            if (i <= myArray.Length - 1)
+            {
+                sum += myArray[i];
+                i++;
+                Recursia(myArray,ref sum, i);
+                
+            }
+          return sum;
+        }
+
+        static void Main()
+        {
+            int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+            int sum = 0;
+            int i = 0;
+            Console.WriteLine(Recursia(myArray,ref sum, i));
+
+        }
+    }
+}
